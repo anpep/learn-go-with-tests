2f36fd5e976500e4fb8c6ea3786f38590db283e9
anpep <ap@anpep.co>
2022-04-27 06:47:50 +0200

    Made docstrings better in array_and_slices package

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,7 +1,7 @@
 // The arrays_and_slices package implements common routines for wokring with arrays and slices.
 package arrays_and_slices
 
-// The Sum function takes a slice of integers and returns the result of adding them together
+// The Sum function takes an array of integers and returns the result of adding them together
 func Sum(integers []int) (sum int) {
 	for _, integer := range integers {
 		sum += integer
@@ -9,6 +9,8 @@ func Sum(integers []int) (sum int) {
 	return
 }
 
+// The SumAll function takes multiple arrays of integers and returns an array with the results of adding the numbers
+// in each array
 func SumAll(integerArrays ...[]int) (sums []int) {
 	for _, array := range integerArrays {
 		sums = append(sums, Sum(array))
@@ -16,6 +18,8 @@ func SumAll(integerArrays ...[]int) (sums []int) {
 	return
 }
 
+// The SumAllTails function takes multiple arrays of integers and returns an array with the results of adding every
+// number but the first in each array
 func SumAllTails(integerArrays ...[]int) (sums []int) {
 	for _, array := range integerArrays {
 		if len(array) == 0 {
