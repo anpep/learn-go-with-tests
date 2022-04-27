72abdf65c45eea4f3013051038a995856f1ea9df
anpep <ap@anpep.co>
2022-04-27 04:57:10 +0200

    Complete "Hello, World" chapter

---
+++ hello.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var helloFormats = map[string]string{
+	"english": "Hello, %s",
+	"spanish": "Hola %s",
+	"french":  "Bonjour %s",
+}
+
+var worlds = map[string]string{
+	"english": "world",
+	"spanish": "mundo",
+	"french":  "monde",
+}
+
+func Hello(name string, lang string) string {
+	if _, doesLangExist := helloFormats[lang]; !doesLangExist {
+		lang = "english"
+	}
+	if name == "" {
+		name = worlds[lang]
+	}
+	return fmt.Sprintf(helloFormats[lang], name)
+}
+
+func main() {
+	fmt.Println(Hello("Go", "spanish"))
+}
---
+++ hello_test.go
@@ -0,0 +1,61 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertMessageIsCorrect := func(t testing.TB, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Ángel", "")
+		want := "Hello, Ángel"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello to people in the default language (english)", func(t *testing.T) {
+		got := Hello("Ángel", "english")
+		want := Hello("Ángel", "")
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello to people in spanish", func(t *testing.T) {
+		got := Hello("Javier", "spanish")
+		want := "Hola Javier"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello to people in french", func(t *testing.T) {
+		got := Hello("Benoît", "french")
+		want := "Bonjour Benoît"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello world by default", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello world in the default language (english)", func(t *testing.T) {
+		got := Hello("", "english")
+		want := Hello("", "")
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello world in spanish", func(t *testing.T) {
+		got := Hello("", "spanish")
+		want := "Hola mundo"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+	t.Run("say hello world in french", func(t *testing.T) {
+		got := Hello("", "french")
+		want := "Bonjour monde"
+		assertMessageIsCorrect(t, got, want)
+	})
+
+}
